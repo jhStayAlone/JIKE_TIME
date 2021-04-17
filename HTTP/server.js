861d56d3be1d94c4b4a4e88a90e0110e5e914bb8
stayAlone <320196806@qq.com>
2021-04-17 09:03:42 +0800

    feat: 浏览器工作原理---HTTP

---
+++ HTTP/server.js
@@ -0,0 +1,17 @@
+const http = require('http')
+
+http.createServer((req, res) => {
+    let body = []
+    req.on('errer', (err) => {
+        console.error(err)
+    }).on('data', (chunk) => {
+        body.push(chunk.toString())
+    }).on('end', () => {
+        body = Buffer.concat(body).toString()
+        console.log(body, 'body')
+        res.writeHead(200, {'content-type': 'text/html'})
+        res.send(' Hello World\n')
+    }).listen(8088)
+})
+
+console.log('服务器已启动!!!')
